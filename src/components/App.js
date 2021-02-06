2a3a90b8e102db95a9c3d4acc6e66d8c822960c2
omarhatem97 <omarhatem221@gmail.com>
2021-02-06 19:49:38 +0200

    fix untracked files

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import { _getUsers } from "../utils/_DATA";
+import react, { Component } from "react";
+
+class App extends Component {
+  // componentDidMount(){
+  //   _getUsers().then((users)=> console.log(users));
+  // }
+
+  render() {
+    return <div>Hello shi</div>;
+  }
+}
+
+export default App;
